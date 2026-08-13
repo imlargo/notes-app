bf5faada343e3c77b719ac3edb2a5427e7781ff2
imlargo <jlargob@unal.edu.co>
2026-08-13 15:20:34 -0500

    refactor: streamline ID generation and improve list return syntax in MockNoteRepository

--- src/services/memory-note-repository.ts
+++ src/services/memory-note-repository.ts
@@ -2,31 +2,30 @@ import { randomColor, type Note } from "../domain/note";
 import { sleep } from "../lib/sleep";
 import type { NoteRepository } from "./repository";
 
-function generateNewID(notes: Note[]): number {
-    return notes.reduce((max, n) => Math.max(max, n.id), 0) + 1;
-}
-
 let _mockNotes: Note[] = [
     { id: 1, text: "Note 1", x: 10, y: 10, w: 150, h: 125 , color: randomColor()},
     { id: 2, text: "Note 2", x: 50, y: 10, w: 150, h: 125 , color: randomColor()},
     { id: 3, text: "Note 3", x: 300, y: 10, w: 150, h: 125, color: randomColor() }
 ]
 
+let _nextId = _mockNotes.reduce((max, n) => Math.max(max, n.id), 0) + 1;
+
 export class MockNoteRepository implements NoteRepository {
 
     async list(): Promise<Note[]> {
         await sleep(500);
 
-        return _mockNotes;
+        return [..._mockNotes];
     }
 
     async create(note: Partial<Note>) {
-        await sleep(500);;
-
         const newNote = {
             ...note,
-            id: generateNewID(_mockNotes),
+            id: _nextId++,
         } as Note
+
+        await sleep(500);
+
         _mockNotes.push(newNote)
         return newNote
     }
