33380ee5073eeb9df89146d15b7d3f8def69eef3
imlargo <jlargob@unal.edu.co>
2026-08-13 11:56:35 -0500

    fix: add className prop to StickyNote component for customizable styling

--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -3,18 +3,21 @@ import type { Note } from "../domain/note"
 
 interface StickyNoteProps {
     note: Note
+    className?: string;
 }
 
 // TODO: primitives and memo
-export function StickyNote({ note }: StickyNoteProps) {
+export function StickyNote({ note, className }: StickyNoteProps) {
     const style: CSSProperties = {
         transform: `translate(${note.x}px, ${note.y}px)`,
         width: note.w,
         height: note.h,
     }
 
-    return <div className="cursor-grab flex flex-col bg-indigo-200 absolute top-10 left-10 border p-4 min-w-12 min-h-24 " style={style} >
-        <textarea className="text-neutral-600 w-full h-full outline-none bg-none" value={note.text}>
+    const cls = "cursor-grab flex flex-col bg-indigo-200 absolute top-0 left-0 border p-4 min-w-12 min-h-24 " + className
+
+    return <div className={cls} style={style} >
+        <textarea className="text-neutral-600 w-full h-full outline-none bg-none" defaultValue={note.text} readOnly>
         </textarea>
     </div>
 }
