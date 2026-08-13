964ef14e269c7066a011cd7b84896d413ab6091d
imlargo <jlargob@unal.edu.co>
2026-08-13 11:11:59 -0500

    fix: remove color property from Note type and update sample notes in MockNoteRepository

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -4,5 +4,4 @@ import type { Rect } from "./geometry"
 export type Note = {
     id: number;
     text: string;
-    color: string;
 } & Rect
--- src/services/note-repository.ts
+++ src/services/note-repository.ts
@@ -5,9 +5,9 @@ export class MockNoteRepository {
     async list(): Promise<Note[]> {
         await this._simulateDelay()
         return [
-            { id: 1, text: "Note 1", color: "red", x: 10, y: 10, w: 150, h: 125 },
-            { id: 2, text: "Note 2", color: "blue", x: 50, y: 10, w: 150, h: 125 },
-            { id: 3, text: "Note 3", color: "green", x: 300, y: 10, w: 150, h: 125 }
+            { id: 1, text: "Note 1", x: 10, y: 10, w: 150, h: 125 },
+            { id: 2, text: "Note 2", x: 50, y: 10, w: 150, h: 125 },
+            { id: 3, text: "Note 3", x: 300, y: 10, w: 150, h: 125 }
         ];
     }
 
