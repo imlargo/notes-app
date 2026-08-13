d107bbfc4887da849e6cdb67c6f6159ced001fd9
imlargo <jlargob@unal.edu.co>
2026-08-13 12:04:20 -0500

    fix: add data-note-id attribute to StickyNote component for improved accessibility

--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -16,7 +16,7 @@ export function StickyNote({ note, className }: StickyNoteProps) {
 
     const cls = "cursor-grab flex flex-col bg-indigo-200 absolute top-0 left-0 border p-4 min-w-12 min-h-24 " + className
 
-    return <div className={cls} style={style} >
+    return <div className={cls} style={style} data-note-id={note.id} >
         <textarea className="text-neutral-600 w-full h-full outline-none bg-none" defaultValue={note.text} readOnly>
         </textarea>
     </div>
