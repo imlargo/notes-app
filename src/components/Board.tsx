7f45821ecad017751dc1d45c7460e06c9b3c662a
imlargo <jlargob@unal.edu.co>
2026-08-13 11:56:44 -0500

    feat: implement pointer events for note creation on the board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Toolbar } from "./Toolbar";
 import type { Note } from "../domain/note";
 import { NoteService } from "../services/note";
 import { MockNoteRepository } from "../services/note-repository";
 import { StickyNote } from "./StickyNote";
-
-
+import { rectFromPoints, type Point, type Rect } from "../domain/geometry";
 
 export function Board() {
     const [notes, setNotes] = useState<Note[]>([]);
+    const [draft, setDraft] = useState<Rect | null>(null)
+
+    const start = useRef<Point | null>(null)
+    const boardOrigin = useRef<Point>({ x: 0, y: 0 })
 
     const noteService = new NoteService(new MockNoteRepository());
 
@@ -16,12 +19,55 @@ export function Board() {
         noteService.getNotes().then((data) => setNotes(data))
     }, [])
 
+    const toLocal = (e: React.PointerEvent): Point => ({
+        x: e.clientX - boardOrigin.current.x,
+        y: e.clientY - boardOrigin.current.y,
+    })
+
+    const onPointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+        const r = e.currentTarget.getBoundingClientRect();
+        boardOrigin.current = { x: r.left, y: r.top }
+        start.current = toLocal(e)
+        e.currentTarget.setPointerCapture(e.pointerId)
+    }
+
+    const onPointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+        if (!start.current) return;
+        setDraft(rectFromPoints(start.current, toLocal(e)))
+    }
+
+    const onPointerUp = (e: React.PointerEvent<HTMLDivElement>) => {
+        if (!start.current) return;
+        const rect = rectFromPoints(start.current, toLocal(e))
+        if (rect.w > 8 && rect.h > 8) {
+            createNote(rect)
+        }
+
+        start.current = null
+        setDraft(null)
+    }
+
+    const boardRef = useRef<HTMLDivElement>(null)
+
+    function createNote(rect: Rect) {
+        noteService.createNote({
+            ...rect
+        })
+    }
+
     return (
-        <div className="canvas-grid w-full h-full relative">
+        <div className="board canvas-grid w-full h-full relative select-none  " ref={boardRef}
+            onPointerDown={onPointerDown}
+            onPointerMove={onPointerMove}
+            onPointerUp={onPointerUp}
+        >
 
             {notes.map((note) => (
-                <StickyNote key={note.id}  note={note} ></StickyNote>
+                <StickyNote key={note.id} note={note} ></StickyNote>
             ))}
+
+
+            {draft && (<StickyNote className="pointer-events-none" note={draft as Note} ></StickyNote>)}
 
             <div className="flex items-center justify-center fixed bottom-6 inset-x-0 pointer-events-none">
                 <Toolbar></Toolbar>
